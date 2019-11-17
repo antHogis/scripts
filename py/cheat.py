1c90762257f9917229f4f9acb54ac5fddd3793db
Anton Höglund <antonjhoglund@hotmail.com>
2019-11-17 15:17:43 +0200

    Print existing cheatsheets in cheats

--- py/cheat.py
+++ py/cheat.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 
+from pathlib import Path, PurePath
+
+CHEATSHEETS_DIR = PurePath.joinpath(Path.home(), '.cheats')
+EXISTING_SHEETS = map(lambda file : str(file).split('/')[-1], \
+    sorted(Path(CHEATSHEETS_DIR).glob('*')))
+
+def list_sheets(): print('\n'.join(EXISTING_SHEETS))
+
+def run():
+    list_sheets()
+
+run()
